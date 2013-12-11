55c19f6f2efbc013d39925372b224a7d769e3a2c
FlashFu <forge451@gmail.com>
2013-12-10 20:02:52 -0600

    removed the desktop code
    removed the desktop code and saved the old files as
    index-with-desktop-controls.html and
    device-orientation-rolling-w-desktop.js

--- rolling-ball/js/device-orientation-rolling-w-desktop.js
+++ rolling-ball/js/device-orientation-rolling-w-desktop.js
@@ -54,7 +54,67 @@ function init(){
 
     cj.Ticker.addEventListener("tick",tick);
     cj.Ticker.setFPS(60);
+    
+    
+    var controller = document.getElementById("controller");
+    var thumb = document.getElementById("thumb");
+    thumb.addEventListener("move", onThumbMove, false);
+    
+    controller.addEventListener("dblclick", doubleClickHandler, false);
+    function doubleClickHandler(e){
+        var cEvent = new Event("change");
+        alpha.value = 0;
+        alpha.dispatchEvent(cEvent);
+        beta.value = 0;
+        beta.dispatchEvent(cEvent);
+        gamma.value = 0;
+        gamma.dispatchEvent(cEvent);
+    }
+    
+    var alpha = document.getElementById("alpha");
+    alpha.addEventListener("change", onChange, false);
+    var beta = document.getElementById("beta");
+    beta.addEventListener("change", onChange, false);
+    var gamma = document.getElementById("gamma");
+    gamma.addEventListener("change", onChange, false);
+}
 
+function onThumbMove(e){
+    var offsetX = (parseFloat(e.left) - 100) * .1;
+    var offsetY = (parseFloat(e.top) - 100) * .1;
+    
+    vx = offsetX;
+    vy = offsetY;
+    console.log("left : " + vx + " top : " + vy);
+
+    var cEvent = new Event("change");
+    beta.value = vx;
+    beta.dispatchEvent(cEvent);
+    gamma.value = vy;
+    gamma.dispatchEvent(cEvent);
+
+};
+
+function onChange(e){
+    switch(e.target.id){
+        case "alpha":
+            alphaOut.value = alpha.value;
+            circle.graphics.beginFill = "blue";
+        break;
+            
+        case "beta":
+            betaOut.value = beta.value;
+            vx = parseFloat(beta.value);
+            console.log("vx: " + vx);
+        break;
+            
+        case "gamma":
+            gammaOut.value = gamma.value;
+            vy = parseFloat(gamma.value);
+            console.log("vy: " + vy);
+        break;
+    }
+}
 
 function tick(){
     if(circle.x > canvas.width){
